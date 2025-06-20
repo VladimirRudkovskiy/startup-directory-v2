df1a2f9d2746639d3bb58cf0de7cd2b9ba3f397f
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-06-20 15:29:54 +0300

    startup details page

---
+++ components/ui/Ping.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Ping = () => {
+	return (
+		<div className='relative'>
+			<div className='absolute -left-4 top-1'>
+				<span className='flex size-[11px]'>
+					<span className='ping-animate'></span>
+					<span className='ping-static'>
+				</span>
+				</span>
+				
+				<span >
+				</span>
+			</div>
+		</div>
+	)
+}
+
+export default Ping
